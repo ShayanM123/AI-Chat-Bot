15bc1df5640fc0572ad5acbf62c4e9169e98abfb
ShayanM123 <111317112+ShayanM123@users.noreply.github.com>
2023-01-05 17:07:38 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const inputField = document.getElementById("input");
+    inputField.addEventListener("keydown", (e) => {
+        if (e.code === "Enter") {
+            let input = inputField.value;
+            inputField.value = "";
+            document.getElementById("Output").innerHTML=input;
+            console.log(input);
+        }
+    });
+})
